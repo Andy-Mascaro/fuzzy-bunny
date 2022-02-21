60b38af1c30d1da5c840bee334a514a8f39adc88
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-21 12:25:55 -0800

    added to fetch utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://kdukidihcdlbgresawun.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkdWtpZGloY2RsYmdyZXNhd3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NDYsImV4cCI6MTk1OTkxNzQ0Nn0.YtwzCCX2zDt5IzpJ-Uh5Hy4DsanDjeq6lfG72ezSCW4';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -9,21 +9,25 @@ export function getUser() {
 }
 
 export async function getFamilies() {
+    const resp = await client.from('loving_families').select().order('id');
+    return checkError(resp);
     // fetch all families and their bunnies
 
-    return checkError(response);
+    
 }
 
 export async function deleteBunny(id) {
+    const resp = await client.from()
     // delete a single bunny using the id argument
 
-    return checkError(response);
+    return checkError(resp);
 }
 
 export async function createBunny(bunny) {
+
     // create a bunny using the bunny argument
 
-    return checkError(response);
+    return checkError(resp);
 }
 
 // MARTHA STEWART (PRE-MADE) FUNCTIONS
@@ -41,15 +45,15 @@ export async function redirectIfLoggedIn() {
 }
 
 export async function signupUser(email, password) {
-    const response = await client.auth.signUp({ email, password });
+    const resp = await client.auth.signUp({ email, password });
 
-    return response.user;
+    return resp.user;
 }
 
 export async function signInUser(email, password) {
-    const response = await client.auth.signIn({ email, password });
+    const resp = await client.auth.signIn({ email, password });
 
-    return response.user;
+    return resp.user;
 }
 
 export async function logout() {
